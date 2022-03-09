35a4eb9a0178bb6777e9cd8a985bac3c5d8017b3
C. Thomas Wyrth <mmxvi.ctw@gmail.com>
2022-03-09 15:54:08 -0800

    First commit, functional component card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Person from './components/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person lastName="Doe" firstName="Jane" age={45} hairColor="Black" />
+      <Person lastName="Smith" firstName="John" age={88} hairColor="Brown" />
+      <Person lastName="Fillmore" firstName="Millard" age={50} hairColor="Brown" />
+      <Person lastName="Smith" firstName="Maria" age={62} hairColor="Blonde" />
     </div>
   );
 }
